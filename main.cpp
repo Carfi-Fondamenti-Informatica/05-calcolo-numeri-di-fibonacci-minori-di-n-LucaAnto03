71c3d0b7a6cc37199bf9d2057d8d4b4936e7a005
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-11-27 21:46:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, a=1, b=0, c=0;
+    cin >> n ;
+    while (c<=n) {
+        c = a + b;
+        a = b;
+        b = c;
+        if (c<=n) {
+            cout << c << endl;
+        }
+    }
+    return 0;
 }
